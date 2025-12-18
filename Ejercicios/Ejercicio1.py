89712c8d49c494c4226af73ba2ef95c15da3c72b
Carlos Garrido <129065902+CarlosGarrido98@users.noreply.github.com>
2025-12-18 09:47:44 +0100

    update ej1

---
+++ Ejercicios/Ejercicio1.py
@@ -0,0 +1,9 @@
+'''
+Ejercicio 1 (Sebastian Becerra)
+Modifica la cadena “ este ejercicio es muy complicado ” para que el ejercicio
+sea facilísimo y cada una de las palabras comience en mayúsculas y sin espacios y luego
+imprimelo invertido, ademas busca el indice de la palabra complicado.
+
+'''
+
+texto = "este ejercicio es muy complicado"
